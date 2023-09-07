88da19619ed7dfac22f432c1d9165cc97ff1d9bf
Ivan <alfaandomega789@gmail.com>
2023-09-07 22:11:50 +0300

    lessons 10 array

---
+++ src/main/java/lessons/lesson11IOThreads/Main.java
@@ -0,0 +1,25 @@
+package lessons.lesson11IOThreads;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+
+ //    BufferedReader reader = new BufferedReader();
+        /*"Hello.txt"*/
+        String s="dss";
+        ArrayList arrayList = new ArrayList<>();
+        Vector vector = new Vector<>();
+        StringBuilder stringBuilder = new StringBuilder("STRF");
+        File file = new File("C://SomeDir");
+
+        System.out.println(file.length());
+
+    }
+
+}
